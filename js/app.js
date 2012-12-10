3625ef2c32d0a32846922826bc343437d101c766
Robert Wawrzyniak <robert-waw@fb4.de>
2012-12-10 17:52:19 +0100

    added app.js for ajax calls and event handling

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: robert
+ * Date: 12/10/12
+ * Time: 5:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function beantragen( ){
+
+}
